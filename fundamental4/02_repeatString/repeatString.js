285d72b48dde0575caf8c9d501154c6584c0e587
musimarcin <marcinm994@gmail.com>
2023-09-27 17:56:16 +0200

    Solved second task.

--- fundamental4/02_repeatString/repeatString.js
+++ fundamental4/02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
 
+    if (times<0) return "ERROR";
+
+    let string = "";
+
+    for (let i = 0; i < times; i++) {
+        string+=text;
+    }
+return string;
 };
 
 // Do not edit below this line
