6af111f364075c99d5c1999df6fb2aa927fab0a6
musimarcin <marcinm994@gmail.com>
2023-09-29 17:57:31 +0200

    Optimized solution

--- fundamental4/06_leapYears/leapYears.js
+++ fundamental4/06_leapYears/leapYears.js
@@ -1,14 +1,7 @@
 const leapYears = function(year) {
 
-    if (year%4 == 0) {
-        if (year%400 == 0 || year%100 !== 0) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
+    return year%4 === 0 && (year%400 ===0 || year%100 !== 0);
+
 };
 
 // Do not edit below this line
