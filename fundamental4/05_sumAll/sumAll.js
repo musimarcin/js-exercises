d032c356a2111c109b7951c40bed722c385b3b2b
musimarcin <marcinm994@gmail.com>
2023-09-29 16:51:20 +0200

    Added solution to fifth task.

--- fundamental4/05_sumAll/sumAll.js
+++ fundamental4/05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(begin, finish) {
+
+    let sum = 0;
+
+    if (begin < 0 || finish < 0 || !Number.isInteger(begin) || !Number.isInteger(finish)) {
+        return "ERROR"
+    } else {
+        if (begin > finish) {
+            for (let i = begin ; i >= finish ; i--) {
+                sum += i;
+            }
+        } else {
+            for (let i = finish ; i >= begin ; i--) {
+                sum += i;
+            }
+        }
+    }
+    return sum;
 
 };
 
