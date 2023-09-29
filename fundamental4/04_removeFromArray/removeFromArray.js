71b0a32a7d91ba67a8d0a21153bef43beca2d3a8
musimarcin <marcinm994@gmail.com>
2023-09-29 16:18:28 +0200

    Change solution to fourth task because it was changing source array.

--- fundamental4/04_removeFromArray/removeFromArray.js
+++ fundamental4/04_removeFromArray/removeFromArray.js
@@ -1,17 +1,14 @@
 const removeFromArray = function(array, ...remove) {
 
-    let found_item = -1;
+    const newArray = [];
 
-    for (const item_to_remove of remove) {
-        for (const item of array) {
-            if (item === item_to_remove) {
-                found_item = array.indexOf(item);
-                array.splice(found_item, 1);
-            }
-        }
+array.forEach((item) => {
+    if (!remove.includes(item)) {
+        newArray.push(item);
     }
+})
 
-return array;
+return newArray;
 
 };
 
