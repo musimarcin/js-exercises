22bc3f120a26372695210f7007bf1e4c4f97b5f0
musimarcin <marcinm994@gmail.com>
2023-09-29 15:55:18 +0200

    Solved third and fourth task.

--- fundamental4/04_removeFromArray/removeFromArray.js
+++ fundamental4/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+
+    let found_item = -1;
+
+    for (const item_to_remove of remove) {
+        for (const item of array) {
+            if (item === item_to_remove) {
+                found_item = array.indexOf(item);
+                array.splice(found_item, 1);
+            }
+        }
+    }
+
+return array;
 
 };
 
