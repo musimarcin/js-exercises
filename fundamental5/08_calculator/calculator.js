01fd6eb6a79b012a3035e7bc54b8b96b54fed1a9
musimarcin <marcinm994@gmail.com>
2023-10-05 17:54:03 +0200

    Updated solution

--- fundamental5/08_calculator/calculator.js
+++ fundamental5/08_calculator/calculator.js
@@ -7,34 +7,24 @@ const subtract = function(x, y) {
 };
 
 const sum = function(array) {
-  let sum = 0;
-	for (const item of array) {
-      sum += item;
-    }
-    return sum;
+    return array.reduce((total, current) => total + current, 0);
 };
 
 const multiply = function(array) {
-  let sum = 1;
-  for (const item of array) {
-    sum *= item;
-  }
-  return sum;
+    return array.reduce((total, current) => total * current);
 };
 
 const power = function(x, y) {
-	return x ** y;
+	return Math.pow(x,y);
 };
 
 const factorial = function(x) {
-    if (x === 0 || x === 1) { return 1;
-    } else {
-        let sum = x;
-        for (let i = (x-1); i > 0; i--) {
-          sum*=i;
-        }
-        return sum;
+    if (x === 0 || x === 1) return 1;
+    let sum = 1;
+    for (let i = x; i > 0; i--) {
+      sum*=i;
     }
+    return sum;
 };
 
 // Do not edit below this line
