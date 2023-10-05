144df0832e3dfd489343e7fddcba79302f064825
musimarcin <marcinm994@gmail.com>
2023-10-05 17:48:21 +0200

    Solved 8th task

--- fundamental5/08_calculator/calculator.js
+++ fundamental5/08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (const item of array) {
+      sum += item;
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = 1;
+  for (const item of array) {
+    sum *= item;
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+    if (x === 0 || x === 1) { return 1;
+    } else {
+        let sum = x;
+        for (let i = (x-1); i > 0; i--) {
+          sum*=i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
