418c062bdaaf88fe8f11025da64f80f4ff8519a1
musimarcin <marcinm994@gmail.com>
2023-10-05 19:51:13 +0200

    Added solution to 10th task

--- fundamental5/10_fibonacci/fibonacci.js
+++ fundamental5/10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    let x = parseInt(fib)
+    if (x < 0) return "OOPS";
+    if (x <= 1) return x;
+    return fibonacci(x - 1) + fibonacci(x - 2);
 };
 
 // Do not edit below this line
