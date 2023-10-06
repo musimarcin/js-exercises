c1db8b90e8da75efedab1a3fdc0a42deb14df666
musimarcin <marcinm994@gmail.com>
2023-10-06 15:58:06 +0200

    Added 12th task solution

--- fundamental5/12_findTheOldest/findTheOldest.js
+++ fundamental5/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((acc, current) => {
+        const accAge = getAge(acc.yearOfBirth, acc.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
 
+        return accAge < currentAge ?  current :  acc;
+        });
+};
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
